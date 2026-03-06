74768f39b7e24fba662670f6c08880978693657d
LP-SWIM <lpswim.info@gmail.com>
2026-03-06 21:43:28 +0100

    app.js

--- app.js
+++ app.js
@@ -244,20 +244,11 @@
             e.preventDefault();
             localStorage.removeItem(STORAGE_KEY); 
             
-            const cookies = document.cookie.split("; ");
-            for (let c = 0; c < cookies.length; c++) {
-                const d = window.location.hostname.split(".");
-                while (d.length > 0) {
-                    const cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
-                    const p = location.pathname.split('/');
-                    document.cookie = cookieBase + '/';
-                    while (p.length > 0) {
-                        document.cookie = cookieBase + p.join('/');
-                        p.pop();
-                    };
-                    d.shift();
-                }
-            }
+            const gaCookies = document.cookie.split(";").filter(c => c.trim().startsWith("_ga"));
+            gaCookies.forEach(cookie => {
+                const name = cookie.split("=")[0].trim();
+                document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; domain=${window.location.hostname.replace('www.','')}`;
+            });
             window.location.reload(); 
         }
 
