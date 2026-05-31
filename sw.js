992aec735fab482681a58a53545b80a0e5161d9e
LP-SWIM <lpswim.info@gmail.com>
2026-05-31 03:23:42 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,15 @@
 'use strict';
-const CACHE_NAME = 'lp-swim-cache-v7';
-const ASSETS_TO_CACHE = [ './', './index.html', './style.css', './app.js', './favicon.webp', './fonts/poppins-v24-latin-regular.woff2', './fonts/poppins-v24-latin-700.woff2', './hintergrund-start.webp' ];
+const CACHE_NAME = 'lp-swim-cache-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './favicon.webp',
+  './fonts/poppins-v24-latin-regular.woff2',
+  './fonts/poppins-v24-latin-700.woff2',
+  './hintergrund-start.webp'
+];
 self.addEventListener('install', (event) => {
   self.skipWaiting(); 
   event.waitUntil(
