28675ff4f3b6918ace47157b2bb9aea2e7dbdb03
LP-SWIM <lpswim.info@gmail.com>
2026-05-31 02:50:45 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE_NAME = 'lp-swim-cache-v12';
+const CACHE_NAME = 'lp-swim-cache-v1';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -7,7 +7,11 @@ const ASSETS_TO_CACHE = [
   './app.js',
   './favicon.webp',
   './fonts/poppins-v24-latin-regular.woff2',
-  './fonts/poppins-v24-latin-700.woff2'
+  './fonts/poppins-v24-latin-700.woff2',
+  './hintergrund-start.webp',
+  './hintergrund-angebote.webp',
+  './hintergrund-standorte.webp',
+  './hintergrund-kontakt.webp'
 ];
 self.addEventListener('install', (event) => {
   self.skipWaiting(); 
