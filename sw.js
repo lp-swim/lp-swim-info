599a5bf868dbf10e7fd9a4302bd295d93e48a75a
LP-SWIM <lpswim.info@gmail.com>
2026-05-31 02:58:50 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE_NAME = 'lp-swim-cache-v1';
+const CACHE_NAME = 'lp-swim-cache-v7';
 const ASSETS_TO_CACHE = [ './', './index.html', './style.css', './app.js', './favicon.webp', './fonts/poppins-v24-latin-regular.woff2', './fonts/poppins-v24-latin-700.woff2', './hintergrund-start.webp' ];
 self.addEventListener('install', (event) => {
   self.skipWaiting(); 
